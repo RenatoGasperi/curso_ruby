5cc57bffed426faa402981081cffb5f1f19c8ecd
RenatoGasperi <99595341+RenatoGasperi@users.noreply.github.com>
2024-02-25 13:26:48 -0300

    Update cadastro_biblioteca.rb

--- cadastro_biblioteca.rb
+++ cadastro_biblioteca.rb
@@ -1,5 +1,12 @@
+puts '--------------------Bem vindo ao cadastro biblioteca------------------------'
+
 print 'Digite seu nome: '
 nome = gets.chomp
+
 print 'Digite seu sobrenome: '
 sobrenome = gets.chomp
+print 'Digite sua idade: '
+idade = gets.chomp.to_i
+
+puts "Bem vindo #{nome} #{sobrenome}, voce tem #{idade} anos."
 
